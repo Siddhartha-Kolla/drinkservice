bb8aaddb2f1e16b016fd2da2b8f143c5973644dc
Siddhartha-Kolla <87278444+Siddhartha-Kolla@users.noreply.github.com>
2024-07-03 15:28:45 +0200

    Added access to shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,36 @@ function App() {
         <Card name={"HohesC Multivitaminsaft C&D"} volume={6} ppl={3.42} img_path={"coca.png"}/>
         <Card name={"HohesC Multivitaminsaft C&D"} volume={6} ppl={3.42} img_path={"coca.png"}/>
       </div>
+      <div class="flex items-center w-full mx-auto justify-center">
+        <button
+            class="group rounded-l-full px-6 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-200 hover:border-gray-300 hover:bg-gray-50">
+            <svg class="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"
+                fill="none">
+                <path d="M16.5 11H5.5" stroke="" stroke-width="1.6" stroke-linecap="round" />
+                <path d="M16.5 11H5.5" stroke="" stroke-opacity="0.2" stroke-width="1.6"
+                    stroke-linecap="round" />
+                <path d="M16.5 11H5.5" stroke="" stroke-opacity="0.2" stroke-width="1.6"
+                    stroke-linecap="round" />
+            </svg>
+        </button>
+        <input type="text"
+            class="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[118px] min-w-[80px] placeholder:text-gray-900 py-[15px] text-center bg-transparent"
+            placeholder="1"/>
+        <button
+            class="group rounded-r-full px-6 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-200 hover:border-gray-300 hover:bg-gray-50">
+            <svg class="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"
+                fill="none">
+                <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" stroke-width="1.6"
+                    stroke-linecap="round" />
+                <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" stroke-opacity="0.2" stroke-width="1.6"
+                    stroke-linecap="round" />
+                <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" stroke-opacity="0.2" stroke-width="1.6"
+                    stroke-linecap="round" />
+            </svg>
+        </button>
+    </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,44 @@
 // import React, { useState } from 'react';
-import './Card.css';
+// import './Card.css';
+// import { FaCartPlus } from "react-icons/fa";
+import * as React from "react"
+ 
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
-const Card = ({name,volume,ppl,img_path}) => {
+const CardComponent = ({id,name,volume,ppl,img_path}) => {
   return (
 
-    <div className="card">
-      <div className='card-img'>
-       <img src={'img/'+img_path} alt="Drink" />
-      </div>
-      <div className='info'>
-        <h1 id='product-name'>{name}</h1>
-        <h2 id='volume'>{volume}L</h2>
-        <div className="price-div">
-          <h3 id='price-pro-litre'>{ppl}€/L</h3>
-          <h2 id='total-price'>{ppl*volume}€</h2>
-        </div>
-        <button id='info-button'>Hinzufügen</button>
-      </div>
-    </div>
-
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm"
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 
 };
 
-export default Card;
+export default CardComponent;
